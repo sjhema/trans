fd96a65531fd133c36407202f795fc1a4f6f78e9
sjhema <sjhema@gmail.com>
2015-12-11 19:03:44 +0530

    Fuel Log

--- src/com/primovision/lutransport/core/util/FuelVendorLogUploadUtil.java
+++ src/com/primovision/lutransport/core/util/FuelVendorLogUploadUtil.java
@@ -2,7 +2,6 @@ package com.primovision.lutransport.core.util;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
