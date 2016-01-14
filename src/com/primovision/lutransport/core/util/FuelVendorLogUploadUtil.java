d29577eda5bcf97b18aff1e55d8a0b8f32e74f8e
Raghav Anantha <raapadma@in.ibm.com>
2016-01-14 22:17:41 +0530

    Fuel

--- src/com/primovision/lutransport/core/util/FuelVendorLogUploadUtil.java
+++ src/com/primovision/lutransport/core/util/FuelVendorLogUploadUtil.java
@@ -518,7 +518,7 @@ public class FuelVendorLogUploadUtil {
 	}
 
 	private static InputStream createInputStream(HSSFWorkbook wb) {
-		dumpToFile(wb);
+		//dumpToFile(wb);
 		
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		try {
