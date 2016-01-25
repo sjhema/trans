b3ed98a279784b53d118609881ded992e2453010
Raghav Anantha <raapadma@in.ibm.com>
2016-01-26 00:24:21 +0530

    Toll sql

--- database/tollUpload/tollUpload.sql
+++ database/tollUpload/tollUpload.sql
@@ -3,3 +3,7 @@ ADD COLUMN `invoice_date` DATETIME NULL DEFAULT NULL COMMENT '';
 
 ALTER TABLE `lutransport`.`tollcompany`
 ADD COLUMN `company_id` BIGINT(20) NULL DEFAULT NULL COMMENT '';
+
+ALTER TABLE `lutransport`.`tollcompany`
+ADD CONSTRAINT `TOLL_COMPANY_LOCAION_FK` FOREIGN KEY (`company_id`) REFERENCES `location` (`id`);
+
