10cf31a965469dbffc9e0b88d14174ff503aa159
Raghav Anantha <raapadma@in.ibm.com>
2016-02-25 17:15:25 +0530

    bereavement

--- database/bereavement/bereavement.sql
+++ database/bereavement/bereavement.sql
@@ -2,3 +2,7 @@
 
 ALTER TABLE `lutransport`.`hourlypayroll_invoice_details` 
 ADD COLUMN `bereavementAmount` DOUBLE NULL DEFAULT NULL;
+
+update `lutransport`.`hourlypayroll_invoice_details`
+set bereavementAmount = 0.0
+where bereavementAmount is null;
